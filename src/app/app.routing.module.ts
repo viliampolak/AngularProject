3c2bc95774bd403ba88aad5fe54d4e8b438858a0
viliampolak <91248882+viliampolak@users.noreply.github.com>
2023-01-19 23:13:48 +0100

    1 file modified, 9 added, 1 deleted

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,18 +6,15 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent,TestComponent,LayoutComponent],
+  declarations: [AppComponent, LoginComponent, TestComponent, LayoutComponent],
   imports: [
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'test', component: TestComponent },
-      { path: '**', redirectTo: 'login' }
-    ])
+      { path: '**', redirectTo: 'login' },
+    ]),
   ],
-  exports: [
-    RouterModule,
-  ],
+  exports: [RouterModule],
   providers: [],
-
 })
 export class AppRoutingModule {}
---
+++ src/app/user/list-user/list-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-user',
+  templateUrl: './list-user.component.html',
+  styleUrls: ['./list-user.component.css']
+})
+export class ListUserComponent {
+
+  constructor() { }
+
+
+}
