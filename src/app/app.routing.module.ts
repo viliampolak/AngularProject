110038d63cf66c2a77da8b08b66b58846faf483d
viliampolak <91248882+viliampolak@users.noreply.github.com>
2023-01-19 23:21:45 +0100

    4 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,9 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { TestComponent } from './test/test.component';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, TestComponent, LayoutComponent],
+  declarations: [AppComponent, LoginComponent, TestComponent, LayoutComponent, AddUserComponent,ListUserComponent],
   imports: [
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
