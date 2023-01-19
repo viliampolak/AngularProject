d66ab300fc1c1a49cba69e2488c4e0210594b000
viliampolak <91248882+viliampolak@users.noreply.github.com>
2023-01-19 19:46:02 +0100

    6 files modified, 10 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  name = 'Viliam';
-  loged = false;
+  //Form variables
+registerForm:any = FormGroup;
+submitted = false;
+constructor( private formBuilder: FormBuilder){}
+//Add user form actions
+get f() { return this.registerForm.controls; }
+onSubmit() {
+  
+  this.submitted = true;
+  // stop here if form is invalid
+  if (this.registerForm.invalid) {
+      return;
+  }
+  //True if all the fields are filled
+  if(this.submitted)
+  {
+    alert("Great!!");
+  }
+ 
+}
+  ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { OneComponent } from './one/one.component';
-import { TwoComponent } from './two/two.component';
-import { LoginComponent } from './login/login.component';
-
-
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, OneComponent, TwoComponent, LoginComponent],
-  bootstrap: [AppComponent, OneComponent, TwoComponent, LoginComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
